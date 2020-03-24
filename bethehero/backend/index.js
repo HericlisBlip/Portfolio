36ed805ca65f572b8ab0a1b9bfbe2fb5107c004b
HericlisBlip <hericlisventura.o@gmail.com>
2020-03-24 11:01:44 -0300

    Construindo a base da aplicação

--- bethehero/backend/index.js
+++ bethehero/backend/index.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
+
+/*
+Rotas e Recursos 
+
+
+Metodos HTTP:
+
+GET: Busca informação no backend
+
+POST: Criar uma informação no backand
+
+PUT: Alterando informação no backend
+
+DELETE: Deletando informação no backend
+
+
+*/
 app.get('/', (request, response) => {
     return response.json({
         evento: 'Semana OmniStack 11.0',
