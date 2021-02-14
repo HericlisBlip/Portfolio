6fcf2740da47d49225f8ec6128dddee390b73b0f
HericlisBlip <hericlisventura.o@gmail.com>
2021-02-14 19:38:40 -0300

    Atualizando estudos sobre javascrit, react.js e next.js

---
+++ estudosReact/app-react/src/componentes/primeiro.jsx
@@ -0,0 +1,17 @@
+import React from  'react'
+
+ export default ( ) => 
+  <>
+    <div className="cabecalho">
+        <h1>Primeiro Componente</h1>
+        <h2> Exemplo de um componente react</h2>
+    </div>
+    <div className="rodape">
+    <h1>Primeiro Componente</h1>
+    <h2>Exemplo de um componente</h2>
+  </div>
+  
+  </>
+
+  
+
---
+++ estudosReact/app-react/src/componentes/ultimo.jsx
@@ -0,0 +1,10 @@
+import  React from 'react';
+
+export default () =>
+<>
+  <div className="rodape">
+    <h1>Primeiro Componente</h1>
+    <h2>Exemplo de um componente</h2>
+  </div>
+
+</>
