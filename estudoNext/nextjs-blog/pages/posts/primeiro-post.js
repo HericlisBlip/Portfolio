e0c7998da440484d94634df35cd2be396194589c
HericlisBlip <hericlisventura.o@gmail.com>
2021-02-28 13:52:21 -0300

    Crian links usando nexts.js

---
+++ estudoNext/nextjs-blog/pages/posts/primeiro-post.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function PrimeiroPost(){
+    return (
+    <>
+    <h1>Primeiro Post</h1>
+        <h2>
+            <Link href="/"><a>Voltar Para Página Index</a></Link>
+        </h2>
+        
+    </> 
+        )
+
+}
